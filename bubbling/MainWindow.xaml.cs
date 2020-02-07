3fa82dd46c6a6d0dc5fbf52674610a9e48ba2744
Randy <38465150+bandydos@users.noreply.github.com>
2020-02-07 14:40:36 +0100

    Add files via upload

---
+++ bubbling/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BubblingDemo_070220
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int _bubblingEventStep;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lblBubbling_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source.Equals(sender))
+            {
+                tbxInfo.Clear();
+                _bubblingEventStep = 1;
+                tbxInfo.Text = "Dit is een bubbling event:\n";
+            }
+            tbxInfo.AppendText(_bubblingEventStep + ": " + ((FrameworkElement)sender).Name + "\n");
+            _bubblingEventStep++;
+
+            //Optioneel => event al handled beschouwen.
+            if(typeof(StackPanel) == sender.GetType())
+            {
+                e.Handled = true; 
+            }
+        }
+    }
+}
---
+++ tunneling/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TunnelingDemo_070220
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int _tunnelingEventStep;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTunneling_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(wndwMain))
+            {
+                tbxInfo.Clear();
+                _tunnelingEventStep = 1;
+            }
+
+            tbxInfo.AppendText(_tunnelingEventStep + ": " +
+                ((FrameworkElement)sender).Name + "\n");
+
+            _tunnelingEventStep++;
+
+            if (e.Source.Equals(sender))
+            {
+                tbxInfo.AppendText("Dit was het tunneling event.\n");
+            }
+        }
+    }
+}
